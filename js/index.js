9fc61894426858ebec29a027d76efc9026ac31f8
graphic <graphic@WFD228-36m.local>
2020-02-13 14:34:19 -0500

    steps so far

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
 // DOCUMENT REFERENCES
-let $doc = document.documentElement  // html element
-let $win = window
-let $bar = document.querySelector('.progress-bar')
+let $doc =    document.documentElement  // html element
+let $win =    window
+let $bar =    document.querySelector('.bar')
+let $prog =   document.querySelector('.progress')
+let $h1 =     document.querySelector('#heading')
+let $h1copy = document.querySelector('.h1copy')
 
 
-// 1. Read Part 1 "Advanced" (Step 1 and 2) and determine which elements need to be selected in addition to the three above
+// STEPS
 
-// 2. Read Step 2 closely and break the request into as many 1-line parts as possible (write the process as a JS comment below)
+// Store the textContent from the $h1
 
+// Apply new textContent to the .h1copy
+
+// Apply opacity: 0 to .h1copy
+
+// Apply opacity: 1 to .h1copy
+
+// Determine when the h1 is off the top of the window
+  // A) Manually scroll, store the window.scrollY
