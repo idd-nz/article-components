48ab576c91a98a7bda7e975c45f70aae2754e173
graphic <graphic@WFD228-36m.local>
2020-02-14 08:48:43 -0500

    First round of steps identified

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 
+
+// Set the display of the .progress-bar to none
+
+// Set the display of the .progress-bar to block
+
+// Store the window height
+
+// Store the document height
+
+// if documentElement height < window height
+
+// ...else...
+
+// How much CAN be scrolled?
+
+// How much HAS been scrolled?
+
+// Determine the amount scrolled as a percentage of the total possible
+
+// Apply a width to the .progress-bar
