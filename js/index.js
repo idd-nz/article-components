32134372fff6cb90936fe6b811a60e3d9e5951ee
graphic <graphic@WFD228-36m.local>
2020-02-14 09:35:44 -0500

    Complete Part 1A

--- js/index.js
+++ js/index.js
@@ -7,35 +7,35 @@ let $bar = document.querySelector('.progress-bar')
 
 // ***** STEPS
 
-// Set the display of the .progress-bar to none
-$bar.style.display = 'none'
-
-// Set the display of the .progress-bar to block
-$bar.style.display = 'block'
-
-// Store the window height
-let winH = $doc.clientHeight    // the document's parent's height
-
-// Store the document height
-let docH = $doc.scrollHeight
-
-// if documentElement height < window height
-if ( docH <= winH ) { console.log('no progress bar needed') }
-
-// ...else...
-else { console.log('i need a progress bar') }
-
-// How much CAN be scrolled?
-let canBeScrolled = docH - winH
-
-// How much HAS been scrolled?
-let hasBeenScrolled = window.scrollY
-
-// Determine the amount scrolled as a percentage of the total possible
-let percentScrolled = hasBeenScrolled / canBeScrolled
-
-// Apply a width to the .progress-bar
-$bar.style.width = `15.56565656465656%`
-
 // Capture the scroll of the window
-$win.addEventListener('scroll', event => { console.log('scrolled!!') })
+$win.addEventListener('scroll', event => { 
+  
+  // Store the window height
+  let winH = $doc.clientHeight    // the document's parent's height
+
+  // Store the document height
+  let docH = $doc.scrollHeight
+
+  // if documentElement height < window height
+  if ( docH <= winH ) { 
+    // Set the display of the .progress-bar to none
+    $bar.style.display = 'none'
+  }
+  else { 
+    // Set the display of the .progress-bar to block
+    $bar.style.display = 'block'
+  }
+
+  // How much CAN be scrolled?
+  let canBeScrolled = docH - winH
+
+  // How much HAS been scrolled?
+  let hasBeenScrolled = window.scrollY
+
+  // Determine the amount scrolled as a percentage of the total possible
+  let percentScrolled = hasBeenScrolled / canBeScrolled
+
+  // Apply a width to the .progress-bar
+  $bar.style.width = `${percentScrolled * 100}%`
+
+})
