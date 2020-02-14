42f5ab7e4952e1cfa554d5bb6434b9c7b7276785
graphic <graphic@WFD228-36m.local>
2020-02-14 10:38:22 -0500

    Next steps complete.

--- js/index.js
+++ js/index.js
@@ -9,6 +9,20 @@ let $ttl = document.querySelector('.title')
 
 // ***** STEPS
 
-/* 
-2. Write all the steps required to complete "Advanced" 2
-*/
+// Get the textContent from the h1
+
+// Set the textContent of the .title
+
+// Set .title opacity to 1
+
+// Set .title opacity to 0
+
+// Get the position of the h1 relative to the window (trigger)
+
+// How much the window has scrolled
+
+// Check if the vanishing point of the h1 is less than the total scrolled
+
+// ...else... 
+
+// Capture the scroll of the window
