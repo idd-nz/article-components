c0b2f93328085bfaacccd21ff05d0bb7e16370af
graphic <graphic@WFD228-36m.local>
2020-02-14 09:29:41 -0500

    Question 3 done

--- js/index.js
+++ js/index.js
@@ -26,9 +26,16 @@ if ( docH <= winH ) { console.log('no progress bar needed') }
 else { console.log('i need a progress bar') }
 
 // How much CAN be scrolled?
+let canBeScrolled = docH - winH
 
 // How much HAS been scrolled?
+let hasBeenScrolled = window.scrollY
 
 // Determine the amount scrolled as a percentage of the total possible
+let percentScrolled = hasBeenScrolled / canBeScrolled
 
 // Apply a width to the .progress-bar
+$bar.style.width = `15.56565656465656%`
+
+// Capture the scroll of the window
+$win.addEventListener('scroll', event => { console.log('scrolled!!') })
